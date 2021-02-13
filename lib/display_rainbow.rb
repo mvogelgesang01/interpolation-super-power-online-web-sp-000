6a58a8594a99ed900368cf8ee29903200593c3b3
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-02-13 18:38:44 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 def #display_rainbow
-  puts display_rainbow(['red', 'orange' 'yellow', 'green', 'blue', 'indigo', 'violet'])
+  puts display_rainbow =(['red', 'orange' 'yellow', 'green', 'blue', 'indigo', 'violet'])
   puts display_rainbow "R: red, O: Orange, Y: yellow, G: green, B: blue, I: Indigo, V: Violet"
 end
